b341cf54ece992d38d31bd1dcc72a86c86b30695
alexei guevara <alexguev@gmail.com>
2012-03-23 17:40:47 -0400

    a

---
+++ src/hg_quatified/core.clj
@@ -0,0 +1,1 @@
+(ns hg-quatified.core)
---
+++ test/hg_quatified/test/core.clj
@@ -0,0 +1,6 @@
+(ns hg-quatified.test.core
+  (:use [hg-quatified.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
