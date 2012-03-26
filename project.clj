1fa5d6c6aa9575d390be7f4c504d047465de01b8
alexei guevara <alexguev@gmail.com>
2012-03-26 17:28:32 -0400

    a

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject hg-quatified "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clj-time "0.3.7"]
+                 [incanter/incanter-core "1.3.0"]
+                 [incanter/incanter-charts "1.3.0"]])
--- src/hg_quatified/core.clj
+++ src/hg_quatified/core.clj
@@ -1,1 +1,26 @@
-(ns hg-quatified.core)
+(ns hg-quatified.core
+  (:use [clj-time.core :only [date-time]]
+        [incanter core stats charts]))
+
+(defn render-code-stats-chart [start-date end-date code-stats]
+  "writes the chart to standard output"
+  )
+
+(defn retrieve-code-stats [start-date end-date]
+  ""
+  {(date-time 2011 01 01) {"AG" {:added 10 :deleted 10}
+                           "KE" {:added 5 :deleted 10}}
+   (date-time 2011 01 02) {"AG" {:added 1 :deleted 5}
+                           "KE" {:added 5 :deleted 1}}
+   (date-time 2011 01 03) {"AG" {:added 7 :deleted 10}
+                           "KE" {:added 5 :deleted 20}}
+   (date-time 2011 01 04) {"AG" {:added 6 :deleted 22}
+                           "KE" {:added 5 :deleted 17}}
+   (date-time 2011 01 05) {"AG" {:added 11 :deleted 1}
+                           "KE" {:added 5 :deleted 1}}
+   (date-time 2011 01 06) {"AG" {:added 12 :deleted 0}
+                           "KE" {:added 5 :deleted 1}}
+   (date-time 2011 01 07) {"AG" {:added 22 :deleted 0}
+                           "KE" {:added 5 :deleted 1}}
+   (date-time 2011 01  8) {"AG" {:added 19 :deleted 0}
+                           "KE" {:added 5 :deleted 1}}})
